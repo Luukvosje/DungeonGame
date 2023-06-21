a42c649fbb34a1eb4d5ce2a4ffb373946c51a408
michal1082 <99263015+michal1082@users.noreply.github.com>
2023-06-21 13:35:34 +0200

    gg
    gg

---
+++ Assets/__Scripts/Shops/BlackSmithShop.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackSmithShop : MonoBehaviour
+{
+
+    public GameObject BlackSmithPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitButton()
+    {
+        BlackSmithPanel.SetActive(false);
+    }
+}
